7a15a77a3a65980620847eac9b31ca01e85656c6
zhibeiyou <xuweiluwx@gmail.com>
2016-04-25 00:18:43 +0800

    添加项目文件。

---
+++ Education/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Education.Startup))]
+namespace Education
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

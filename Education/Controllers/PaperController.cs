2a4574a0f87dae848e82d064f75570e01a3728d4
zhibeiyou <xuweiluwx@gmail.com>
2016-04-27 00:48:21 +0800

    add first controller

--- Education/Controllers/PaperController.cs
+++ Education/Controllers/PaperController.cs
@@ -4,15 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Education.Models;
+using PagedList;
+using Education.Abstract;
+using Education.Concrete;
 
 namespace Education.Controllers
 {
     public class PaperController : BaseController
     {
-        // GET: Paper
-        public ActionResult Index()
+        private const int PageSize = 10;
+        private IEntityRepository<Paper> repository;
+        public PaperController(IEntityRepository<Paper> repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult Index(int page = 1)
+        {
+            return View(DB.Papers.OrderByDescending(p => p.EditOn).ToPagedList(page, PageSize));
+        }
+
+        public ActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Paper paper)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Add(paper);
+            }
         }
     }
 }

33f4acf558afde447e0a8aad19fa675ac9286593
zhibeiyou <xuweiluwx@gmail.com>
2016-04-27 18:23:37 +0800

    Finish the create controller of papaer

--- Education/Controllers/PaperController.cs
+++ Education/Controllers/PaperController.cs
@@ -7,6 +7,8 @@ using Education.Models;
 using PagedList;
 using Education.Abstract;
 using Education.Concrete;
+using System.Threading.Tasks;
+using Education.ViewModels;
 
 namespace Education.Controllers
 {
@@ -19,7 +21,7 @@ namespace Education.Controllers
             this.repository = repository;
         }
 
-        public ActionResult Index(int page = 1)
+        public ActionResult List(int page = 1)
         {
             return View(DB.Papers.OrderByDescending(p => p.EditOn).ToPagedList(page, PageSize));
         }
@@ -28,13 +30,66 @@ namespace Education.Controllers
         {
             return View();
         }
+
         [HttpPost]
-        public ActionResult Create(Paper paper)
+        public async Task<ActionResult> Create(PaperViewModel paperInfo)
         {
             if (ModelState.IsValid)
             {
-                repository.Add(paper);
-            }
+                var teacher = await GetCurrentUserAsync() as Teacher;
+                Paper paper = new Paper();
+                paper.EditOn = DateTime.Now;
+                paper.Teacher = teacher;
+                var trueOrFalseQuestionList = new List<TrueOrFalseQuestion>();
+                foreach(TrueOrFalseQuestionViewModel questioninfo in paperInfo.TrueOrFalseQuestions)
+                {
+                    trueOrFalseQuestionList.Add(new TrueOrFalseQuestion
+                    {
+                        Type = QuestionType.判断题,
+                        Content = questioninfo.Content,
+                        IsCorrect = questioninfo.IsCorrect
+                    });
+                };
+                var singleQuestionList = new List<ChoiceQuestion>();
+                foreach(SingleQuestionViewModel questioninfo in paperInfo.SingleQuestions)
+                {
+                    ChoiceQuestion c = new ChoiceQuestion();
+                    c.Type = QuestionType.单选题;
+                    c.Content = questioninfo.Content;
+                    foreach(OptionViewModel optioninfo in questioninfo.Options)
+                    {
+                        c.Options.Add(new Option
+                        {
+                            OptionProperty = optioninfo.OptionProperty,
+                            IsCorrect = optioninfo.IsCorrect,
+                        });
+                    }
+                    singleQuestionList.Add(c);
+                }
+                var multipleQuestionList = new List<ChoiceQuestion>();
+                foreach (MultipleQuestionViewModel questioninfo in paperInfo.MultipleQuestions)
+                {
+                    ChoiceQuestion c = new ChoiceQuestion();
+                    c.Type = QuestionType.多选题;
+                    c.Content = questioninfo.Content;
+                    foreach (OptionViewModel optioninfo in questioninfo.Options)
+                    {
+                        c.Options.Add(new Option
+                        {
+                            OptionProperty = optioninfo.OptionProperty,
+                            IsCorrect = optioninfo.IsCorrect,
+                        });
+                    }
+                    multipleQuestionList.Add(c);
+                }
+                paper.Questions.AddRange(trueOrFalseQuestionList);
+                paper.Questions.AddRange(singleQuestionList);
+                paper.Questions.AddRange(multipleQuestionList);
+                DB.Papers.Add(paper);
+                await DB.SaveChangesAsync();
+                return View();
+            };
+            return View(paperInfo);
         }
     }
 }
---
+++ Education/ViewModels/PaperViewModels.cs
@@ -0,0 +1,65 @@
+﻿using Education.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Education.ViewModels
+{
+    public class PaperViewModel
+    {
+        public List<TrueOrFalseQuestionViewModel> TrueOrFalseQuestions { get; set; }
+        public List<SingleQuestionViewModel> SingleQuestions { get; set; }
+        public List<MultipleQuestionViewModel> MultipleQuestions { get; set; }
+        public PaperViewModel()
+        {
+            TrueOrFalseQuestions = new List<TrueOrFalseQuestionViewModel>();
+            SingleQuestions = new List<SingleQuestionViewModel>();
+            MultipleQuestions = new List<MultipleQuestionViewModel>();
+        }
+    }
+    public abstract class QuestionViewModel
+    {
+        [DisplayName("题目类型")]
+        public QuestionType Type { get; set; }
+
+        [DisplayName("题目内容")]
+        [Required(ErrorMessage = "请输入题目内容！")]
+        public string Content { get; set; }
+    }
+    public class TrueOrFalseQuestionViewModel : QuestionViewModel
+    {
+        public bool IsCorrect { get; set; }
+    }
+    public class SingleQuestionViewModel : QuestionViewModel
+    {
+        public List<OptionViewModel> Options { get; set; }
+        public SingleQuestionViewModel()
+        {
+            Options = new List<OptionViewModel>();
+        }
+    }
+    public class MultipleQuestionViewModel : QuestionViewModel
+    {
+        public List<OptionViewModel> Options { get; set; }
+        public MultipleQuestionViewModel()
+        {
+            Options = new List<OptionViewModel>();
+        }
+    }
+
+    public class OptionViewModel
+    {
+        [DisplayName("选项号")]
+        public OptionType OptiondId { get; set; }
+
+        [DisplayName("选项内容")]
+        [Required(ErrorMessage = "请输入选项内容！")]
+        public string OptionProperty { get; set; }
+
+        [DisplayName("选项是否正确？")]
+        public bool IsCorrect { get; set; }
+    }
+}
